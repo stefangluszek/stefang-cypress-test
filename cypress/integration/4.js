7ce9415aea14fd308fcf9347e7ae0f52c6a1ec15
Stefan Gluszek <stefang@kahoot.com>
2022-06-02 15:47:03 +0200

    Add 4.

--- cypress/integration/4.js
+++ cypress/integration/4.js
@@ -1,6 +1,7 @@
-describe('My Second Test', () => {
+describe('My 4 Test', () => {
   it('Visits the Kitchen Sink', () => {
     cy.visit('https://example.cypress.io')
     cy.wait(10000)
   })
 })
+
